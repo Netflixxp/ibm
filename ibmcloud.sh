a0828745ccd1c8851590f2735f45c20d212d64a4
Netflixxp <61532011+Netflixxp@users.noreply.github.com>
2020-09-15 10:52:53 +0800

    9月15日更新

--- ibmcloud.sh
+++ ibmcloud.sh
@@ -4,10 +4,9 @@ read -p "请设置你的容器内存大小(默认256):" ramsize
 if [ -z "$ramsize" ];then
 	ramsize=256
 fi
-rm -rf cloudfoundry
-mkdir cloudfoundry
-cd cloudfoundry
-
+rm -rf phpcf
+mkdir phpcf
+cd phpcf
 echo '<!DOCTYPE html> '>>index.php
 echo '<html> '>>index.php
 echo '<body>'>>index.php
@@ -16,32 +15,32 @@ echo 'echo "Hello World!"; '>>index.php
 echo '?> '>>index.php
 echo '<body>'>>index.php
 echo '</html>'>>index.php
-
-wget https://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip
-unzip -d v2ray1 v2ray-linux-64.zip
-cd v2ray1
-chmod 777 *
-cd ..
-rm -rf v2ray-linux-64.zip
-mv $HOME/cloudfoundry/v2ray1/v2ray $HOME/cloudfoundry/v2ray
-mv $HOME/cloudfoundry/v2ray1/v2ctl $HOME/cloudfoundry/v2ctl
-rm -rf $HOME/cloudfoundry/v2ray1
-uuid=`cat /proc/sys/kernel/random/uuid`
-path=`echo $uuid | cut -f1 -d'-'`
-echo '{"inbounds":[{"port":8080,"protocol":"vmess","settings":{"clients":[{"id":"'$uuid'","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/'$path'"}}}],"outbounds":[{"protocol":"freedom","settings":{}}]}'>$HOME/cloudfoundry/config.json
+wget https://www.armn1.ml/entrypoint.sh
+chmod +x entrypoint.sh
 echo 'applications:'>>manifest.yml
 echo '- path: .'>>manifest.yml
-echo '  command: '/app/htdocs/v2ray'' >>manifest.yml
+echo '  command: '/app/htdocs/entrypoint.sh'' >>manifest.yml
 echo '  name: '$appname''>>manifest.yml
 echo '  random-route: true'>>manifest.yml
 echo '  memory: '$ramsize'M'>>manifest.yml
 ibmcloud target --cf
 ibmcloud cf push
-domain=`ibmcloud cf app $appname | grep routes | cut -f2 -d':' | sed 's/ //g'`
-vmess=`echo '{"add":"'$domain'","aid":"64","host":"","id":"'$uuid'","net":"ws","path":"/'$path'","port":"443","ps":"IBM_Cloud","tls":"tls","type":"none","v":"2"}' | base64 -w 0`
-cd ..
-echo 容器已经成功启动
-echo 地址: $domain
-echo UUID: $uuid
-echo path: /$path
-echo vmess://$vmess
+ibmyuming=$(ibmcloud app show $appname | grep h |awk '{print $2}'| awk -F: 'NR==2{print}')
+    VMESSCODE=$(base64 -w 0 << EOF
+    {
+      "v": "2",
+      "ps": "v2ws IBM",
+      "add": "$ibmyuming",
+      "port": "443",
+      "id": "8e82a9a5-b894-4b4d-bf2e-dbf0807572aa",
+      "aid": "4",
+      "net": "ws",
+      "type": "none",
+      "host": "",
+      "path": "/ws",
+      "tls": "tls"
+    }
+EOF
+    )
+	echo "配置链接："
+    echo vmess://${VMESSCODE}
