8dcd8d9adfd8fef59c75e8fe62852c77945d1a48
Netflixxp <61532011+Netflixxp@users.noreply.github.com>
2020-09-15 18:48:06 +0800

    更新

--- ibmv2ws.sh
+++ ibmv2ws.sh
@@ -15,7 +15,7 @@ echo 'echo "Hello World!"; '>>index.php
 echo '?> '>>index.php
 echo '<body>'>>index.php
 echo '</html>'>>index.php
-wget https://github.com/Netflixxp/ibm/blob/master/entrypoint.sh
+wget https://www.armn1.ml/entrypoint.sh
 chmod +x entrypoint.sh
 echo 'applications:'>>manifest.yml
 echo '- path: .'>>manifest.yml
